baa451061b8631b6dc3c5c1277a05652fb78219e
Farhad <farhad.hsni@gmail.com>
2018-12-15 21:04:31 +0330

    GenServer

--- test/bucket_test.exs
+++ test/bucket_test.exs
@@ -2,7 +2,8 @@ defmodule KV.BucketTest do
   use ExUnit.Case, async: true
 
   setup do
-    {:ok, bucket} = KV.Bucket.start_link([])
+    bucket = start_supervised!(KV.Bucket)
+
     %{bucket: bucket}
   end
 
